39bbbf6c467f6c3a18c3d240d552b3246a08a53b
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-07-29 12:28:13 +0900

    Commit for Render redeployment

--- Part03/backend_test/index.js
+++ Part03/backend_test/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 
+const app = express()
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -82,7 +85,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
