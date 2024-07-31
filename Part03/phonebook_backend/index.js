3ad9c89eabf52a1b0413de6bfc7eb5c12e36f58f
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-07-31 11:54:49 +0900

    Backend deployment commit

--- Part03/phonebook_backend/index.js
+++ Part03/phonebook_backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 morgan.token('content', (req, res) => {
     const bd = JSON.stringify(req.body)
     return bd
