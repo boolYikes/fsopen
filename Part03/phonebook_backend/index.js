4f63d7901a26c7c891ebc45ad47bdae798749ad5
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-07-31 12:05:08 +0900

    uibuild

--- Part03/phonebook_backend/index.js
+++ Part03/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('content', (req, res) => {
     const bd = JSON.stringify(req.body)
     return bd
