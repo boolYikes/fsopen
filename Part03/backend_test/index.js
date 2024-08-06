6b66325d9f7b2b4e93495565c4518eaccda9027c
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-08-06 10:56:41 +0900

    Commit before the exercise

--- Part03/backend_test/index.js
+++ Part03/backend_test/index.js
@@ -19,23 +19,23 @@ const requestLogger = (request, response, next) => {
     next()
 }
 app.use(requestLogger)
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only JavaScript",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         important: true
+//     }
+// ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello Warudo!!</h1>')
@@ -46,43 +46,38 @@ app.get('/api/notes', (request, response) => {
     })
 })
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id == id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    }else{
-        response.status(404).end()
-    }
+    })
 })
 app.delete('/api/notes/:id', (request, response) => {
     const id = request.params.id
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
 })
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//         ? Math.max(...notes.map(n => Number(n.id)))
+//         : 0
+//     return String(maxId + 1)
+// }
 app.post(`/api/notes`, (request, response) => {
     const body = request.body
     
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 // handle non-exsistant route with middleware
