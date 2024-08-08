e5e0bde69ebfa70e527d1afc037e0c87974473a4
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-08-08 11:21:39 +0900

    Course material commit

--- Part03/backend_test/index.js
+++ Part03/backend_test/index.js
@@ -45,36 +45,52 @@ app.get('/api/notes', (request, response) => {
         response.json(notes)
     })
 })
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        // console.log(request.params.id)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        // console.log(error)
+        // response.status(400).send({error: 'malformatted ID'})
+        next(error)
     })
 })
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-// const generateId = () => {
-//     const maxId = notes.length > 0
-//         ? Math.max(...notes.map(n => Number(n.id)))
-//         : 0
-//     return String(maxId + 1)
-// }
+// update importance
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
 app.post(`/api/notes`, (request, response) => {
     const body = request.body
-    
     if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
-
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
-
     note.save().then(savedNote => {
         response.json(savedNote)
     })
@@ -83,10 +99,18 @@ app.post(`/api/notes`, (request, response) => {
 // handle non-exsistant route with middleware
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'UNKNOWN ENDPOINT'})
-    // no next method cuz no more middleware?
+    // no next method cuz no more middleware? EDIT: NOPE It simply doesn't need it
 }
 app.use(unknownEndpoint)
-
+// handle error
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return express.response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001 // this is for deploying on host services
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
