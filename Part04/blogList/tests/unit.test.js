b2dbfd6dd9415843eb969127233b9d22a1282c04
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-08-16 10:03:23 +0900

    Part04 Exercise 4.4 commit

--- Part04/blogList/tests/unit.test.js
+++ Part04/blogList/tests/unit.test.js
@@ -2,9 +2,49 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('', () => {
+test('and returns 1', () => {
     const blogs = []
-
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    const blogs0 = []
+    const blogs1 = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const blogs2 = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '6a422aa71b54a676234d17f8',
+            title: 'Test title',
+            author: 'Test author',
+            url: 'https://test.url',
+            likes: 11,
+            __v: 0
+        }
+    ]
+    test('Zero element -> returns zero', () => {
+        assert.strictEqual(listHelper.totalLikes(blogs0), 0)
+    })
+    test('Only one element -> return its likes straight up', () => {
+        assert.strictEqual(listHelper.totalLikes(blogs1), 5)
+    })
+    test('Multiple elements -> return the sum proper', () => {
+        assert.strictEqual(listHelper.totalLikes(blogs2), 16)
+    })
 })
--- Part04/blogList/utils/list_helper.js
+++ Part04/blogList/utils/list_helper.js
@@ -2,7 +2,11 @@ const dummy = (blogs) => {
     console.log(`Dummy recieving ${blogs}`)
     return 1
 }
+const totalLikes = (blogs) => {
+    return blogs.reduce((prev, x) => x.likes + prev, 0)
+}
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
