175a2eba3b715fd06a9524f7249d44c465601be6
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-08-16 11:36:23 +0900

    Part04 Exercise 4.5 commit

--- Part04/blogList/utils/list_helper.js
+++ Part04/blogList/utils/list_helper.js
@@ -5,8 +5,16 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((prev, x) => x.likes + prev, 0)
 }
+const favoriteBlog = (blogs) => {
+    const result = blogs.reduce((prev, curr) => {
+        return prev.likes < curr.likes ? curr : prev
+    }, blogs[0])
+    // console.log(`The result is : ${result}`)
+    return result
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
