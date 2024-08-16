4a943f2b68cc875fe9cf821622f9374551829aa4
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-08-16 11:38:33 +0900

    Part04 Exercise 4.5 edit

--- Part04/blogList/tests/unit.test.js
+++ Part04/blogList/tests/unit.test.js
@@ -49,42 +49,44 @@ describe('total likes', () => {
     })
 })
 
-test('The most loved blog.', () => {
-    const blogs2 = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '6a422aa71b54a676234d17f8',
-            title: 'Test title',
-            author: 'Test author',
-            url: 'https://test.url',
-            likes: 11,
-            __v: 0
-        },
-        {
-            _id: '7a422aa71b54a676234d17f8',
-            title: 'Test2 title',
-            author: 'Test2 author',
-            url: 'https://test2.url',
-            likes: 8,
-            __v: 0
-        }
-    ]
-    assert.deepStrictEqual(
-        listHelper.favoriteBlog(blogs2),
-        {
-            _id: '6a422aa71b54a676234d17f8',
-            title: 'Test title',
-            author: 'Test author',
-            url: 'https://test.url',
-            likes: 11,
-            __v: 0
-        }
-    )
+describe('Favorite blog', () => {
+    test('The most loved blog is...', () => {
+        const blogs2 = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 5,
+                __v: 0
+            },
+            {
+                _id: '6a422aa71b54a676234d17f8',
+                title: 'Test title',
+                author: 'Test author',
+                url: 'https://test.url',
+                likes: 11,
+                __v: 0
+            },
+            {
+                _id: '7a422aa71b54a676234d17f8',
+                title: 'Test2 title',
+                author: 'Test2 author',
+                url: 'https://test2.url',
+                likes: 8,
+                __v: 0
+            }
+        ]
+        assert.deepStrictEqual(
+            listHelper.favoriteBlog(blogs2),
+            {
+                _id: '6a422aa71b54a676234d17f8',
+                title: 'Test title',
+                author: 'Test author',
+                url: 'https://test.url',
+                likes: 11,
+                __v: 0
+            }
+        )
+    })
 })
